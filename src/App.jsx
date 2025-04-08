c837e16fc44e91a27920a6b6a920b5a74f4e58b4
ght17a <hubert.truong01@gmail.com>
2025-04-08 23:46:33 +0200

    Add raw data mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <MovieCard movie={{title: "Hubz's film", release_date: "2024"}}/>
-      <MovieCard movie={{title: "Die's film", release_date: "2023"}}/>
+      <Home />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "2020" },
+    { id: 3, title: "The Matrix", release_date: "2020" },
+  ];
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+      </form>
+      <button type="submit" className="search-button">Search</button>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
